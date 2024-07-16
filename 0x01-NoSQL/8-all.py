1231f5b071bc558c4d54c16efc55ef2c42b7290a
agutuomondi <agutudennis1997@gmail.com>
2024-07-16 13:42:24 -0700

    0x01. NoSQL

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def list_all(mongo_collection):
+
+    doc_list = []
+
+    for doc in mongo_collection.find():
+        if doc:
+            doc_list.append(doc)
+    return doc_list
